38bb0081423e6e9c5ba5b5a47531c51b9f0fba82
Diego V <diego.v@bineo.com>
2022-08-31 11:03:00 -0500

    ajustar columnas

--- scripts/update_cyber.py
+++ scripts/update_cyber.py
@@ -1,6 +1,5 @@
 
 from src.utilities import tools
-
 from config import SITE
 
 #%% Preparación
@@ -8,11 +7,12 @@ cyber_fields = SITE/"refs/catalogs/Cyber.xlsm.lnk"
 
 dtypes_dict = {'bool': 'bool', 'char': 'string', 'int': 'int64'}
 sap_cols_1 = tools.read_excel_table(cyber_fields, 'Output', 'output_cols')
-sap_cols = (sap_cols_1[sap_cols_1['leer'].notnull()]
-    .assign(**{'leer': lambda df: df['leer'].replace(dtypes_dict)}))
-dtypes_cols = {row['columnas']: row['leer'] for _, row in sap_cols.iterrows()}
+sap_cols = (sap_cols_1[sap_cols_1['proc'].notnull()]
+    .assign(**{'proc': lambda df: df['proc'].replace(dtypes_dict)}))
+dtypes_cols = {row['columnas']: row['proc'] for _, row in sap_cols.iterrows()}
 
-
+weird_bools = { a_col: (lambda df: df[a_col] == 1)
+    for a_col, a_type in dtypes_cols.items() if a_type == 'bool'}
 
 cyber_types = tools.read_excel_table(cyber_fields, 'Output', 'tipos_datos')
 
@@ -20,15 +20,34 @@ cyber_types = tools.read_excel_table(cyber_fields, 'Output', 'tipos_datos')
 sap_tables  = tools.read_excel_table(cyber_fields, 'Output', 'tablas_sap')
 tables_dict = {row['tabla_origen']: row['datalake'] for _, row in sap_tables.iterrows()}
 
+
 #%% Ejecución
 pre_attributes = tools.read_excel_table(cyber_fields, 'Output', 'output')
-which_attributes = (pre_attributes['Mandatorio']==1) | pre_attributes['tabla_origen'].notnull()
-sap_attributes   = (pre_attributes
-    .loc[which_attributes, sap_cols['columnas']]
-    .astype(dtypes_cols)
-    .assign(**{
-        'tabla_origen': lambda df: df['tabla_origen'].replace(tables_dict)})
-    .reset_index())
 
-sap_attributes.to_feather("refs/catalogs/cyber_columns.feather")
+weird_bools = { a_col: lambda df: df[a_col] == 1
+    for a_col, a_type in dtypes_cols.items() if a_type == 'bool'}
+
+weirder_bools2 = {a_col: (lambda df: df[a_col] == 1) for a_col in ['Mandatorio', ]}  #, 'tiene_calculo', 'check_NA', 'check_size'
+# Por alguna razón WEIRD_BOOLS no funciona. 
+
+sap_attributes = (pre_attributes[sap_cols['columnas']]
+    .assign(**weirder_bools2 )
+    .astype(dtypes_cols)
+    .replace({'tabla_origen': tables_dict}))
+
+#%% Checks
+non_checks = (sap_attributes
+    .assign(**{'check_mandatorio': lambda df: 
+            ~df['Mandatorio'] | df['tabla_origen'].notnull()})
+    .filter(like='check', axis=1)
+    .apply(lambda lgl_srs: sum(~lgl_srs), axis=0))
+
+print(non_checks)
+
+#%% And print. 
+sub_columns = ['nobmre', 'Posición inicial', 'Longitud', 'valor_fijo', 'Tipo de dato', 'tabla_origen', ]
+post_attributes = (sap_attributes
+    .loc[sap_attributes['tabla_origen'].notnull()]
+    .reset_index())
+post_attributes.to_feather("refs/catalogs/cyber_columns.feather")
 
