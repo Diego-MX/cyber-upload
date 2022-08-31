84c6d13305d58aa91fb5f3bbea7c89864ee44abd
Diego V <diego.v@bineo.com>
2022-08-31 14:29:27 -0500

    actualizar columnas

--- scripts/update_cyber.py
+++ scripts/update_cyber.py
@@ -5,17 +5,14 @@ from config import SITE
 #%% Preparación
 cyber_fields = SITE/"refs/catalogs/Cyber.xlsm.lnk"
 
-dtypes_dict = {'bool': 'bool', 'char': 'string', 'int': 'int64'}
 sap_cols_1 = tools.read_excel_table(cyber_fields, 'Output', 'output_cols')
+dtypes_dict = {'bool': 'bool', 'char': 'string', 'int': 'int64'}
 sap_cols = (sap_cols_1[sap_cols_1['proc'].notnull()]
     .assign(**{'proc': lambda df: df['proc'].replace(dtypes_dict)}))
 dtypes_cols = {row['columnas']: row['proc'] for _, row in sap_cols.iterrows()}
-
-weird_bools = { a_col: (lambda df: df[a_col] == 1)
-    for a_col, a_type in dtypes_cols.items() if a_type == 'bool'}
+weird_bools = [a_col for a_col, a_type in dtypes_cols.items() if a_type == 'bool']
 
 cyber_types = tools.read_excel_table(cyber_fields, 'Output', 'tipos_datos')
-
 
 sap_tables  = tools.read_excel_table(cyber_fields, 'Output', 'tablas_sap')
 tables_dict = {row['tabla_origen']: row['datalake'] for _, row in sap_tables.iterrows()}
@@ -24,14 +21,8 @@ tables_dict = {row['tabla_origen']: row['datalake'] for _, row in sap_tables.ite
 #%% Ejecución
 pre_attributes = tools.read_excel_table(cyber_fields, 'Output', 'output')
 
-weird_bools = { a_col: lambda df: df[a_col] == 1
-    for a_col, a_type in dtypes_cols.items() if a_type == 'bool'}
-
-weirder_bools2 = {a_col: (lambda df: df[a_col] == 1) for a_col in ['Mandatorio', ]}  #, 'tiene_calculo', 'check_NA', 'check_size'
-# Por alguna razón WEIRD_BOOLS no funciona. 
-
 sap_attributes = (pre_attributes[sap_cols['columnas']]
-    .assign(**weirder_bools2 )
+    .apply(lambda a_col: (a_col == 1) if a_col.name in weird_bools else a_col)
     .astype(dtypes_cols)
     .replace({'tabla_origen': tables_dict}))
 
