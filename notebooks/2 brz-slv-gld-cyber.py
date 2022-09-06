ad248c35750c11263396454b292d7b80fb12c2df
diego.v@bineo.com <diego.v@bineo.com>
2022-09-06 22:10:35 +0000

    Megapulir el notebook de SLV-GLD-CYBER para archivos.

--- notebooks/2 brz-slv-gld-cyber.py
+++ notebooks/2 brz-slv-gld-cyber.py
@@ -33,6 +33,10 @@ az_manager.set_dbks_permissions(at_storage)
 # Sustituye el placeholder AT_STORAGE, aunque mantiene STAGE para sustituirse después. 
 base_location = f"abfss://{{stage}}@{at_storage}.dfs.core.windows.net/ops/core-banking-batch-updates"
 
+def pd_print(a_df: pd.DataFrame, width=180): 
+    with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', width):
+        print(a_df)
+
 
 # COMMAND ----------
 
@@ -42,7 +46,22 @@ base_location = f"abfss://{{stage}}@{at_storage}.dfs.core.windows.net/ops/core-b
 # COMMAND ----------
 
 # MAGIC %md
-# MAGIC ## Solo 3 requieren modificación 
+# MAGIC ## Solo 4 requieren modificación  
+# MAGIC 
+# MAGIC * `Loans Contract`:  es para filtrar
+# MAGIC * `Person Set`: tiene una modificación de dirección
+# MAGIC * `Balances`, `Open Items`: sí se tiene que analizar a fondo
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ### Loan Contract (Contract Set)
+
+# COMMAND ----------
+
+loan_contract = (spark.read.table(tables['brz_loans'])
+    .filter(F.col('LifeCycleStatusTxt') == 'activos, utilizados'))
+display(loan_contract)
 
 # COMMAND ----------
 
@@ -145,18 +164,6 @@ display(uncleared)
 
 # COMMAND ----------
 
-from typing import List
-from pyspark.sql.dataframe import DataFrame as SparkDataFrame
-from pyspark.sql.column import Column as SparkColumn
-
-def select_ls_from_dict(rename_dict: dict) -> List[SparkColumn]: 
-    the_list = [F.col(vv).alias(kk) for kk, vv in rename_dict.items()]
-    return the_list
-
-
-
-# COMMAND ----------
-
 persons_name    = "bronze.persons_set"
 loans_name      = "bronze.loan_contracts"    
 lqan_name       = "bronze.loan_qan_contracts"
@@ -165,10 +172,46 @@ opens_name      = "bronze.loan_open_items"
 
 tables_dict = {
     "bronze.persons_set"        : persons, 
-    "bronze.loan_contracts"     : spark.read.table("bronze.loan_contracts"), 
+    "bronze.loan_contracts"     : loan_contract, 
     "bronze.loan_qan_contracts" : spark.read.table("bronze.loan_qan_contracts"),
     "bronze.loan_balances"      : balances,
     "bronze.loan_open_items"    : uncleared}
+
+
+# COMMAND ----------
+
+# MAGIC %md 
+# MAGIC ### Tablas individuales
+
+# COMMAND ----------
+
+the_sap_tbls = pd.read_feather("../refs/catalogs/cyber_sap.feather")
+the_sap_tbls
+
+# COMMAND ----------
+
+# Only on SAP_COLS
+
+# joins_dict = {a_row['tabla_origen']: dict(a_row['join_cols'].split(',')) 
+#               for _, a_row in the_sap_tbls.iterrows()}
+
+persons_select  = [F.col(a_row['la_columna']).alias(a_row['nombre']) 
+        for _, a_row in sap_cols.iterrows() if a_row['tabla_origen'] == 'bronze.persons_set']
+loans_select    = [F.col(a_row['la_columna']).alias(a_row['nombre']) 
+        for _, a_row in sap_cols.iterrows() if a_row['tabla_origen'] ==  'bronze.loan_contracts']
+lqan_select     = [F.col(a_row['la_columna']).alias(a_row['nombre']) 
+        for _, a_row in sap_cols.iterrows() if a_row['tabla_origen'] == 'bronze.loan_qan_contracts']
+balances_select = [F.col(a_row['la_columna']).alias(a_row['nombre']) 
+        for _, a_row in sap_cols.iterrows() if a_row['tabla_origen'] == 'bronze.loan_balances']
+opens_select    = [F.col(a_row['la_columna']).alias(a_row['nombre']) 
+        for _, a_row in sap_cols.iterrows() if a_row['tabla_origen'] ==  'bronze.loan_open_items']
+
+# With Join columns. 
+persons_tbl  = tables_dict['bronze.persons_set'].select(*persons_select, F.col('ID').alias('person_id'))
+loans_tbl    = tables_dict['bronze.loan_contracts'].select(*loans_select, F.col('ID').alias('loan_id'), F.col('BorrowerID').alias('person_id'))
+lqan_tbl     = tables_dict['bronze.loan_qan_contracts'].select(*lqan_select, F.col('ID').alias('loan_id'))
+balances_tbl = tables_dict['bronze.loan_balances'].select(*balances_select, F.col('ID').alias('loan_id'))
+opens_tbl    = tables_dict['bronze.loan_open_items'].select(*opens_select, F.col('ContractID').alias('loan_id'))
 
 
 
@@ -179,60 +222,42 @@ tables_dict = {
 
 # COMMAND ----------
 
-sap_cols_0 = pd.read_feather("../refs/catalogs/cyber_columns.feather")
+all_cols_1 = (pd.read_feather("../refs/catalogs/cyber_columns.feather")
+    .drop(columns=['index']))
 
-campo = sap_cols_0['campo']
-tipo_campo = pd.Series(np.where(campo.isnull(), 1,
-                    np.where(campo.notnull() & (campo.str.slice(stop=1) != '='), 2, 3)))
-            
-sap_cols_1 = (sap_cols_0
+valor_fijo = all_cols_1['valor_fijo']
+campo      = all_cols_1['campo']
+tipo_calc  = all_cols_1['tipo_calc']
+ref_col    = all_cols_1['ref_col']
+
+all_cols_0 = (all_cols_1
     .assign(**{
-        'tipo_campo': tipo_campo, 
-        'la_columna': campo.where(tipo_campo.isin([1, 2]), 
-                sap_cols_0['calc'].str.extract(r"\(.*, (.*)\)", expand=False))}))
+        'tabla_origen': all_cols_1['tabla_origen'].fillna(''), 
+        'valor_fijo': np.where((valor_fijo == 'N/A') | valor_fijo.isnull(), None, valor_fijo), 
+        'la_columna': np.where(tipo_calc == 1, campo, 
+                    np.where(tipo_calc == 2, ref_col.str.extract(r"\(.*, (.*)\)", expand=False), 
+                  np.where(tipo_calc == 3, None, None)))}))
 
-sap_cols_1['in_sap'] = np.nan
+all_cols_0['in_sap'] = False
 for name in tables_dict: 
-    sub_idx = (sap_cols_1['tabla_origen'] == name).isin([True])
-    sap_cols_1.loc[sub_idx, 'in_sap'] = \
-            sap_cols_1['la_columna'][sub_idx].isin(tables_dict[name].columns)
+    sub_idx = (all_cols_1['tabla_origen'] == name).isin([True])
+    all_cols_0.loc[sub_idx, 'in_sap'] = \
+            all_cols_0['la_columna'][sub_idx].isin(tables_dict[name].columns)
 
-keep_rows = (sap_cols_1['tipo_campo'] == 1) | sap_cols_1['in_sap'] 
-sap_cols = sap_cols_1[keep_rows]
+all_cols = all_cols_0.assign(**{
+    'spk_type' : np.where(all_cols_0['Tipo de dato'] == 'VARCHAR2', 'str', 
+               np.where(all_cols_0['Tipo de dato'] == 'DATE', 'date', 
+             np.where(all_cols_0['decimales'] > 0, 'dbl', 'int')))})
 
-#cols_1 = ['nombre', 'valor_fijo', 'tabla_origen', 'campo', 'calc', 'tipo_campo', 'la_columna'] 
-cols_1 = ['nombre', 'valor_fijo', 'longitud2', 'decimales', ] #'tipo_campo', 'la_columna'
+sap_cols = all_cols[all_cols['in_sap']]
+
+# [ 'nombre', 'Posición inicial', 'longitud2', 'decimales', 'valor_fijo',
+#   'Tipo de dato', 'tabla_origen', 'campo', 'calc', 'tipo_calc', 'ref_col']
+# += ['la_columna', 'in_sap'] 
+cols_1 = ['nombre', 'tipo_calc', 'Tipo de dato', 'valor_fijo', 'la_columna', 'in_sap', 'spk_type'] 
 with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 220):
-    # print(sap_cols[cols_1])
-    # print(sap_cols.loc[sap_cols['tabla_origen'] == 'bronze.loan_contracts', cols_1])
-    print(sap_cols_0.loc[sap_cols_0['valor_fijo'].notnull() & (sap_cols_0['Tipo de dato'] == 'NUMBER'), cols_1])
-
-
-# COMMAND ----------
-
-# MAGIC %md 
-# MAGIC ### Tablas individuales
-
-# COMMAND ----------
-
-persons_select  = {a_row['nombre']: a_row['la_columna'] 
-        for _, a_row in sap_cols[sap_cols['tabla_origen'] == 'bronze.persons_set'].iterrows()}
-loans_select    = {a_row['nombre']: a_row['la_columna'] 
-        for _, a_row in sap_cols[sap_cols['tabla_origen'] == 'bronze.loan_contracts'].iterrows()}
-lqan_select     = {a_row['nombre']: a_row['la_columna'] 
-        for _, a_row in sap_cols[sap_cols['tabla_origen'] == 'bronze.loan_qan_contracts'].iterrows()}
-balances_select = {a_row['nombre']: a_row['la_columna'] 
-        for _, a_row in sap_cols[sap_cols['tabla_origen'] == 'bronze.loan_balances'].iterrows()}
-opens_select    = {a_row['nombre']: a_row['la_columna'] 
-        for _, a_row in sap_cols[sap_cols['tabla_origen'] == 'bronze.loan_open_items'].iterrows()}
-
-# With Join columns. 
-persons_tbl  = tables_dict['bronze.persons_set'].select(*select_ls_from_dict(persons_select), F.col('ID').alias('person_id'))
-loans_tbl    = tables_dict['bronze.loan_contracts'].select(*select_ls_from_dict(loans_select), F.col('ID').alias('loan_id'), F.col('BorrowerID').alias('person_id'))
-lqan_tbl     = tables_dict['bronze.loan_qan_contracts'].select(*select_ls_from_dict(lqan_select), F.col('ID').alias('loan_id'))
-balances_tbl = tables_dict['bronze.loan_balances'].select(*select_ls_from_dict(balances_select), F.col('ID').alias('loan_id'))
-opens_tbl    = tables_dict['bronze.loan_open_items'].select(*select_ls_from_dict(opens_select), F.col('ContractID').alias('loan_id'))
-
+    print(all_cols.loc[all_cols['tipo_calc'] == 3, cols_1])
+    
 
 
 # COMMAND ----------
@@ -242,11 +267,30 @@ opens_tbl    = tables_dict['bronze.loan_open_items'].select(*select_ls_from_dict
 
 # COMMAND ----------
 
-fixed_slct = [F.lit(a_row['valor_fijo']).alias(a_row['nombre'])
-    for _, a_row in sap_cols[sap_cols['tipo_campo'] == 1].iterrows()]
+cast_types = {
+    'str' : T.StringType(), 
+    'int' : T.IntegerType(), 
+    'dbl' : T.DoubleType(), 
+    'date': T.DateType()
+}
+# VALOR_FIJO tiene 'N/A' != None. 
 
-fix_chars = sap_cols['nombre'][
-    (sap_cols['tipo_campo'] == 1) & (sap_cols['Tipo de dato'] == 'VARCHAR2')]
+fixed_slct = [ F.lit(a_row['valor_fijo'])
+                .cast(cast_types[a_row['spk_type']])
+                .alias(a_row['nombre'])
+    for _, a_row in all_cols[all_cols['tipo_calc'] == 3].iterrows()]
+
+missing_cols = [F.lit(None).alias(a_row['nombre'])
+    for _, a_row in all_cols[all_cols['tipo_calc'] == 4].iterrows()]
+
+missing_sap = [F.lit(None).alias(a_row['nombre'])
+    for _, a_row in all_cols[all_cols['tipo_calc'].isin([1, 2]) & ~all_cols['in_sap']].iterrows()]
+
+null_as_zeros = [a_row['nombre'] for _, a_row in all_cols.iterrows() 
+            if (a_row['spk_type'] in ['dbl', 'int'])]
+null_as_blank = [a_row['nombre'] for _, a_row in all_cols.iterrows() 
+            if (a_row['spk_type'] in ['date', 'str'])]
+
 
 golden_1 = (persons_tbl
     .join(loans_tbl   , how='right', on='person_id')
@@ -254,11 +298,11 @@ golden_1 = (persons_tbl
     .join(balances_tbl, how='left' , on='loan_id') 
     .join(opens_tbl   , how='left' , on='loan_id')
     .drop(*['person_id', 'loan_id'])
-    .select('*', *fixed_slct)
-    .fillna('N/A', fix_chars.tolist()))
+    .select('*', *missing_cols, *missing_sap, *fixed_slct)
+    .fillna(0, subset=null_as_zeros)
+    .fillna('', subset=null_as_blank))
 
 display(golden_1)
-
 
 # COMMAND ----------
 
@@ -267,80 +311,65 @@ display(golden_1)
 
 # COMMAND ----------
 
-fstring_keys = {
-    'str': 's', 'date': 's', 'int': 'd', 'dbl': 'f'
-}
+longitud_f = (all_cols['longitud2'].astype('float') 
+            + all_cols['decimales']/10).astype(str)
+longitud_i =  all_cols['longitud2'].astype(str)
 
-sap_cols_fmt_0 = sap_cols.assign(**{
-    'spk_type' : np.where(sap_cols['Tipo de dato'] == 'VARCHAR2', 'str', 
-               np.where(sap_cols['Tipo de dato'] == 'DATE', 'date', 
-             np.where(sap_cols['decimales'] > 0, 'dbl', 'int')))})
+pre_format = np.where(all_cols['spk_type'] == 'str', longitud_i + 's', 
+             np.where(all_cols['spk_type'] == 'date',longitud_i + 's', 
+             np.where(all_cols['spk_type'] == 'dbl', longitud_f + 'f', '0' + longitud_i + 'd')))
 
-fill_zeros   = pd.Series(np.where(sap_cols_fmt_0['Tipo de dato'] == 'NUMBER', '0', ''))
-fill_space   = pd.Series(np.where(sap_cols_fmt_0['Tipo de dato'] == 'NUMBER', ' ', ''))
-old_longitud = (sap_cols_fmt_0['longitud2'] + sap_cols_fmt_0['decimales']).apply(str)
-its_fkey     = pd.Series([fstring_keys[a_spk] for a_spk in sap_cols_fmt_0['spk_type']])
-smp_longitud = sap_cols_fmt_0['longitud2'].apply(str)
+all_cols_fmt = all_cols.assign(**{
+    'f_string' : '%' + pre_format,
+    's_string' : '%' + longitud_i + 's'}) 
 
-
-sap_cols_fmt = sap_cols_fmt_0.assign(**{
-    'f_string' : '%' + fill_zeros + old_longitud + its_fkey, 
-    's_string' : '%' + smp_longitud + 's'}) 
-    
+# [ 'nombre', 'Posición inicial', 'longitud2', 'decimales', 'valor_fijo',
+#   'Tipo de dato', 'tabla_origen', 'campo', 'calc', 'tipo_calc', 'ref_col',
+#   'la_columna', 'in_sap', 'spk_type']
+# ... 'f_string', 's_string']
 fmt_cols = ['nombre', 'Posición inicial', 'longitud2', 'decimales', 'spk_type', 'Tipo de dato', 'f_string', 's_string']
 with pd.option_context('display.max_rows', None, 'display.max_columns', None, 'display.width', 220):
-    print(sap_cols_fmt.loc[sap_cols_fmt['Tipo de dato'] == 'NUMBER', fmt_cols])
+    print(all_cols_fmt.loc[all_cols_fmt['Tipo de dato'] == 'NUMBER', fmt_cols])
     
-
-# COMMAND ----------
-
-int_select
 
 # COMMAND ----------
 
 # The types:  
-# 1. Make sure to cast first. 
+# 1. Let's cast to type first ;) 
 # 2. Each type is converted to string in its terms. 
 # 3. Apply fixed width condition ...
 # 4. Select columns in order. 
 
-cast_types = {
-    'str' : T.StringType(), 
-    'int' : T.DoubleType(), 
-    'dbl' : T.DoubleType(), 
-    'date': T.DateType()
-}
 
 ## 1. 
-types_select = [F.col(a_row['nombre']).cast(cast_types[a_row['spk_type']]).alias(a_row['nombre'])
-    for _, a_row in sap_cols_fmt.iterrows()]
-
-## 2 and 3.  
+types_select = [
+    F.col(a_row['nombre']).cast(cast_types[a_row['spk_type']]).alias(a_row['nombre'])
+        for _, a_row in all_cols_fmt.iterrows()]
+## 2. 
 str_select = [F.col(a_row['nombre']) 
-    for _, a_row in sap_cols_fmt.iterrows() if a_row['spk_type'] == 'str']
+    for _, a_row in all_cols_fmt.iterrows() if a_row['spk_type'] == 'str']
 
 date_select = [F.date_format(a_row['nombre'], 'MMddyyyy').alias(a_row['nombre']) 
-    for _, a_row in sap_cols_fmt.iterrows() if a_row['spk_type'] == 'date']
+    for _, a_row in all_cols_fmt.iterrows() if a_row['spk_type'] == 'date']
 
-#---  Assume decimales < 10. 
-dbl_select = [F.number_format(a_row['nombre'], int(10*a_row['decimales'])).alias(a_row['nombre']) 
-    for _, a_row in sap_cols_fmt.iterrows() if a_row['spk_type'] == 'dbl']
+dbl_select = [F.format_number(a_row['nombre'], int(a_row['decimales'])).alias(a_row['nombre']) 
+    for _, a_row in all_cols_fmt.iterrows() if a_row['spk_type'] == 'dbl']
 
 # int_select = [F.format_string(str(a_row['f_string']), a_row['nombre']).alias(a_row['nombre'])
 #     for _, a_row in sap_cols_fmt.iterrows() if a_row['spk_type'] == 'int']
 
 int_select = [F.format_string(str(a_row['f_string']), a_row['nombre']).alias(a_row['nombre'])
-    for _, a_row in sap_cols_fmt.iterrows() if a_row['spk_type'] == 'int']
-
+    for _, a_row in all_cols_fmt.iterrows() if a_row['spk_type'] == 'int']
 
 ## 3. 
 fxd_width_select = [F.format_string(str(a_row['s_string']), F.col(a_row['nombre'])).alias(a_row['nombre'])
-    for _, a_row in sap_cols_fmt.iterrows()]
+    for _, a_row in all_cols_fmt.iterrows()]
 
 golden_2 = (golden_1.select(*types_select)
-    .select(*str_select, *dbl_select, *date_select) )  #  , *int_select
-#    .select(*fxd_width_select))
-#    .select(F.concat(*sap_cols_fmt['nombre']).alias('one_mega_string')))
-#    .select(*sap_cols_fmt['nombre'], F.concat(*sap_cols_fmt['nombre']).alias('one_mega_string')))
+    .select(*int_select, *str_select, *dbl_select, *date_select)   
+    .select(*fxd_width_select)
+    .select(F.concat(*sap_cols_fmt['nombre']).alias('one_mega_string'))
+#    .select(*sap_cols_fmt['nombre'], F.concat(*sap_cols_fmt['nombre']).alias('one_mega_string'))
+)
 
 display(golden_2)
