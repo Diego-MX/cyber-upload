8c8ccb26dcce48b4231e6ab891bcf5a391d40ddc
Diego Villamil <diego.v@bineo.com>
2023-07-17 12:39:08 -0600

    Agregar archivo user-databricks.yml

--- refs/scripts/commands.ps1
+++ refs/scripts/commands.ps1
@@ -7,3 +7,6 @@ databricks --profile <qas> jobs --version 2.1 create --json-file .\refs\dbks_job
 $env:ENV_TYPE = 'qas'; $env:SERVER_TYPE='local'; ipython 
 
 
+$env:GITHUB_TOKEN = 'PASTE-TOKEN'
+databricks --profile <qas> secrets create-scope <user-diego-villamil>
+databricks --profile <qas> secrets put --scope <user-diego-villamil> --key <github-access-token> --string-value $env:GITHUB_TOKEN
